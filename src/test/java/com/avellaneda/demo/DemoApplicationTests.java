129b310f46b13c9e6d53aba7f94b85b134db277e
maxi <maxi19@gmail.com>
2022-02-27 13:26:32 -0300

    initial commit

---
+++ src/test/java/com/avellaneda/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avellaneda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
